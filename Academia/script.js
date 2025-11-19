1b93a6fab40911bb53cf35a1405cb9140148967e
Renan Sousa Ramos <212819199+Renan-Ramos26@users.noreply.github.com>
2025-11-18 21:30:59 -0300

    feito

---
+++ Academia/script.js
@@ -0,0 +1,6 @@
+// Aqui você pode adicionar funcionalidades extras com JavaScript se necessário.
+// Por exemplo, uma interação simples ao clicar no botão de WhatsApp ou localização.
+
+document.querySelector('.btn-whatsapp').addEventListener('click', function() {
+    alert('Você será redirecionada para o WhatsApp!');
+});
